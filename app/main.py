7e1f3c4567151d65f39fe55dc7dd5133812b79a7
Ivan <d1034182016@gmail.com>
2025-03-24 16:46:43 +0800

    chore: api response orjson lib instead of json lib.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from logging import Logger
 from logging import getLogger
+from typing import Any
 
+import orjson
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 # from fastapi.responses import ORJSONResponse
 from fastapi.routing import APIRoute
+from pydantic import BaseModel
 
 from app.apis import route
 
@@ -20,11 +24,24 @@ def custom_generate_unique_id(route: APIRoute) -> str:
     return route.name
 
 
+class CustomORJSONResponse(ORJSONResponse):
+    def render(self, content: Any) -> bytes:
+        if isinstance(content, dict):
+            content = orjson.dumps(content, option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SERIALIZE_NUMPY)
+
+            return content
+
+        if isinstance(content, BaseModel):
+            content = content.model_dump()
+
+        return super().render(content)
+
+
 app: FastAPI = FastAPI(
     title=settings.project.name,
     lifespan=lifespan,
     generate_unique_id_function=custom_generate_unique_id,
-    # default_response_class=ORJSONResponse,
+    default_response_class=CustomORJSONResponse,
     swagger_ui_parameters={
         "persistAuthorization": True,
         "filter": True,
