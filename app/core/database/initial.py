d080756759eafc60ed15b43d019f4ef42055e119
Ivan <d1034182016@gmail.com>
2025-03-24 14:31:47 +0800

    feat: add init example URL.

--- app/core/database/initial.py
+++ app/core/database/initial.py
@@ -1,7 +1,22 @@
+from datetime import UTC
+
+import arrow
 from sqlmodel import Session
+
+from app.apis.utils import utils
+from app.models.urls import UrlsBase
+
+EXAMPLE_URL = "https://www.google.com"
 
 
 async def inject_initial_data(*, session: Session) -> None:
-    ...
+    url_obj: UrlsBase = UrlsBase(
+        original_url=EXAMPLE_URL,
+        short_url=utils.generate_url_uuid(EXAMPLE_URL),
+        expiration_date=arrow.utcnow().shift(days=30).astimezone(UTC),
+    )
 
-    # session.commit()
+    session.add(url_obj)
+    session.commit()
+
+    session.refresh(url_obj)
